acccfaecac5d618f61ec7084a84b652206b963d9
Brian McPhillips <B.McPhillips89@gmail.com>
2022-04-18 19:54:11 -0700

    EventList presentation component

---
+++ src/components/EventList/EventList.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Event from '../Event/Event';
+
+const EventList = ({ events }) => {
+  const eventStuff = events.map(event => (
+    <li key={`${event.year} - ${event.text}`}>
+      <Event {...event}/>
+    </li>
+  ));
+
+  return (
+    <ul>
+      {eventStuff}
+    </ul>
+  );
+};
+
+EventList.propTypes = {
+  events: PropTypes.arrayOf(PropTypes.shape({
+    year: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired
+  })).isRequired
+};
+
+export default EventList;
