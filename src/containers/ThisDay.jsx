5a6000171459fb1689a402d6257abd945158f937
Brian McPhillips <B.McPhillips89@gmail.com>
2022-04-21 00:28:28 -0700

    Items rendered to screen

--- src/containers/ThisDay.jsx
+++ src/containers/ThisDay.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import EventList from '../components/EventList/EventList';
+import { getThisDay } from '../services/thisDay-api';
 
 const ThisDay = () => {
   const [loading, setLoading] = useState(true); 
   const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    getThisDay()
+      .then(events => setEvents(events))
+      .finally(() => setLoading(false));
+  }, []);
 
   if(loading) return <h1 data-testid="loading">Loading...</h1>;
   return (
